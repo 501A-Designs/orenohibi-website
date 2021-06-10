5a4591fc78a4aeabe4ef41f4175d9e8331bf8a39
501A <art71903design.gmail.com>
2021-06-10 22:48:10 +0900

    removed modal, added new images & pages

--- component.js
+++ component.js
@@ -11,6 +11,202 @@
 const template = document.createElement('template');
 
 // CONTENT CARD
+// template.innerHTML = `
+// <style>
+// h3{
+//     font-size: var(--h3);
+//     margin: var(--margin);
+// }
+// .imgCardClass {
+//   position: relative;
+//   animation:combackAni 1s;
+// }
+// img{
+//   transition: 0.5s;
+//   width: 100%;
+//   height: 100%;
+//   object-fit: cover;
+//   border-radius:5px;
+// }
+// #imgBlur{
+//   position: absolute;
+//   transform: scale(1.02,1.02);
+//   background-color:radial-gradient(86.36% 107.55% at 6.49% 12.32%, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 0.5) 100%);
+//   z-index: -1;
+//   background-size: cover;
+//   border-radius: 5px;
+//   object-fit: cover;
+//   opacity:0.5;
+//   filter: blur(20px);
+//   margin-top:-95%;
+//   width:100%;
+//   height:90%;
+//   transition:0.5s;
+// }
+// p{
+//     margin:0px;
+//     padding: var(--padding);
+//     margin-top:-14px;
+// }
+// .imgCardClass img:hover{
+//     cursor: pointer;
+//     transform:scale3d(0.95, 0.95, 0.95);
+// }
+// .imgCardClass:hover #imgBlur{
+//   opacity:0.5;
+//   filter: blur(15px);
+//   height:95%;
+// }
+
+// .popUp {
+//     box-shadow: 5px 5px 10px grey;
+//     z-index: 2;
+//     background-color:white;
+//     position: fixed;
+//     overflow-y: auto;
+//     overflow-x:hidden;
+//     padding:var(--padding);
+//     transform: scale3d(1, 1, 1);
+//     border-radius:5px;
+//     transition:1s;
+//     animation: popUpAni 1s;
+// }
+//     button{
+//         outline:none;
+//         border: 2px solid black;
+//         background-color: transparent;
+//         color: black;
+//         padding: 1em;
+//         padding-top: 0.5em;
+//         padding-bottom: 0.5em;
+//         transition: 0.5s;
+//         font-weight:bold;
+//         margin: 0px;
+//         float:right;
+//         z-index:3;
+//     }
+//     button:hover{
+//       cursor:pointer;
+//       background-color: black;
+//       color: var(--orenoGreen);
+//     }
+//     .popUp div{
+//         display: grid;
+//         gap: var(--margin);
+//     }
+//     section button{
+//         display:none;
+//     }
+      
+//     @keyframes combackAni{
+//         0%{
+//             box-shadow: 5px 5px 10px light;
+//             backdrop-filter: blur(0px);
+//             border-radius: 5px;
+//             top:50px;
+//             transform: scale3d(0.5, 0.5, 0.5);
+//         }
+//         35%{
+//             top:0px;
+//             transform: scale3d(1.05, 1.05, 1.05);
+//         }
+//     }
+//     @keyframes popUpAni{
+//         0%{
+//             transform-style: preserve-3d;
+//             transform: scale3d(0.1, 0.1, 0.1) rotateX(-80deg) rotateY(0deg) rotateZ(0deg);
+//             box-shadow: 5px 5px 10px light;
+//             backdrop-filter: blur(0px);
+//             border-radius: 5px;
+//             top:50px;
+//         }
+//         35%{
+//             transform: scale3d(1.2, 1.2, 1.2) rotateX(20deg) rotateY(0deg) rotateZ(0deg);
+//         }
+//     }
+
+//     @media only screen and (max-width: 400px) {
+//         .popUp{
+//             width: 80%;
+//             top: calc(10% - 25px);
+//             right: calc(13% - 25px);
+//             bottom: calc(10% - 25px);
+//         }
+//         .popUp div{
+//             grid-template-columns: 1fr;
+//         }
+//     }
+//     @media only screen and (min-width: 401px) and (max-width: 600px) {
+//         .popUp{
+//             width: 75%;
+//             top: calc(15% - 25px);
+//             right: calc(15% - 25px);
+//             bottom: calc(15% - 25px);
+//         }
+//         .popUp div{
+//             grid-template-columns: 1fr;
+//         }
+//     }
+//     @media only screen and (min-width: 601px) {
+//         .popUp{
+//             width: 50%;
+//             top: calc(25% - 25px);
+//             right: calc(25% - 25px);
+//             max-height: 800px;
+//         }
+//         .popUp div{
+//             grid-template-columns: 1fr 1fr;
+//         }
+//     }    
+//   </style>
+//   <section class="imgCardClass">
+//   <button>CLOSE</button>
+//     <h3></h3>
+//     <div>
+//     <img alt="No Image" />
+//     <p style="display:none"><slot></slot></p>
+//     </div>
+//     <div id="imgBlur"></div>
+//   </section>
+// `;
+// class imgCard extends HTMLElement {
+//   constructor() {
+//     super();
+//     this.attachShadow({ mode: 'open' });
+//     this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+//     this.shadowRoot.querySelector('h3').innerText = this.getAttribute('header');
+//     // this.shadowRoot.querySelector('p').innerText = this.innerHTML;
+      
+//     if (this.getAttribute('header') === null) {
+//       this.shadowRoot.querySelector('h1').remove();
+//     }
+      
+//     const section = this.shadowRoot.querySelector('section');
+//     const btn = this.shadowRoot.querySelector('button');
+//     const p = this.shadowRoot.querySelector('p');
+//     const imgHref = this.getAttribute('imgHref');
+//     const img = this.shadowRoot.querySelector('img');
+//     const imgBlur = this.shadowRoot.getElementById('imgBlur');
+
+//     img.setAttribute('src', `${imgHref}`);
+//     imgBlur.setAttribute('style', `background-image: url("${imgHref}");`);
+      
+//     img.addEventListener('click', function() {
+//         section.setAttribute('class', 'popUp');
+//         btn.setAttribute('style', 'display:block;');
+//         p.setAttribute('style', 'display:block;');
+//     })
+//     btn.addEventListener('click', function() {
+//         section.setAttribute('class', 'imgCardClass');
+//         btn.setAttribute('style', 'display:none;');
+//         p.setAttribute('style', 'display:none;');
+//     })
+//   }
+// }
+// window.customElements.define('img-card', imgCard);
+
+
 template.innerHTML = `
 <style>
 h3{
@@ -43,11 +239,7 @@ img{
   height:90%;
   transition:0.5s;
 }
-p{
-    margin:0px;
-    padding: var(--padding);
-    margin-top:-14px;
-}
+
 .imgCardClass img:hover{
     cursor: pointer;
     transform:scale3d(0.95, 0.95, 0.95);
@@ -56,115 +248,25 @@ p{
   opacity:0.5;
   filter: blur(15px);
   height:95%;
+}      
+@keyframes combackAni{
+    0%{
+        box-shadow: 5px 5px 10px light;
+        backdrop-filter: blur(0px);
+        border-radius: 5px;
+        top:50px;
+        transform: scale3d(0.5, 0.5, 0.5);
+    }
+    35%{
+        top:0px;
+        transform: scale3d(1.05, 1.05, 1.05);
+    }
 }
-
-.popUp {
-    box-shadow: 5px 5px 10px grey;
-    z-index: 2;
-    background-color:white;
-    position: fixed;
-    overflow-y: auto;
-    overflow-x:hidden;
-    padding:var(--padding);
-    transform: scale3d(1, 1, 1);
-    border-radius:5px;
-    transition:1s;
-    animation: popUpAni 1s;
-}
-    button{
-        outline:none;
-        border: 2px solid black;
-        background-color: transparent;
-        color: black;
-        padding: 1em;
-        padding-top: 0.5em;
-        padding-bottom: 0.5em;
-        transition: 0.5s;
-        font-weight:bold;
-        margin: 0px;
-        float:right;
-        z-index:3;
-    }
-    button:hover{
-      cursor:pointer;
-      background-color: black;
-      color: var(--orenoGreen);
-    }
-    .popUp div{
-        display: grid;
-        gap: var(--margin);
-    }
-    section button{
-        display:none;
-    }
-      
-    @keyframes combackAni{
-        0%{
-            box-shadow: 5px 5px 10px light;
-            backdrop-filter: blur(0px);
-            border-radius: 5px;
-            top:50px;
-            transform: scale3d(0.5, 0.5, 0.5);
-        }
-        35%{
-            top:0px;
-            transform: scale3d(1.05, 1.05, 1.05);
-        }
-    }
-    @keyframes popUpAni{
-        0%{
-            transform-style: preserve-3d;
-            transform: scale3d(0.1, 0.1, 0.1) rotateX(-80deg) rotateY(0deg) rotateZ(0deg);
-            box-shadow: 5px 5px 10px light;
-            backdrop-filter: blur(0px);
-            border-radius: 5px;
-            top:50px;
-        }
-        35%{
-            transform: scale3d(1.2, 1.2, 1.2) rotateX(20deg) rotateY(0deg) rotateZ(0deg);
-        }
-    }
-
-    @media only screen and (max-width: 400px) {
-        .popUp{
-            width: 80%;
-            top: calc(10% - 25px);
-            right: calc(13% - 25px);
-            bottom: calc(10% - 25px);
-        }
-        .popUp div{
-            grid-template-columns: 1fr;
-        }
-    }
-    @media only screen and (min-width: 401px) and (max-width: 600px) {
-        .popUp{
-            width: 75%;
-            top: calc(15% - 25px);
-            right: calc(15% - 25px);
-            bottom: calc(15% - 25px);
-        }
-        .popUp div{
-            grid-template-columns: 1fr;
-        }
-    }
-    @media only screen and (min-width: 601px) {
-        .popUp{
-            width: 50%;
-            top: calc(25% - 25px);
-            right: calc(25% - 25px);
-            max-height: 800px;
-        }
-        .popUp div{
-            grid-template-columns: 1fr 1fr;
-        }
-    }    
   </style>
   <section class="imgCardClass">
-  <button>CLOSE</button>
     <h3></h3>
     <div>
     <img alt="No Image" />
-    <p style="display:none"><slot></slot></p>
     </div>
     <div id="imgBlur"></div>
   </section>
@@ -183,25 +285,14 @@ class imgCard extends HTMLElement {
     }
       
     const section = this.shadowRoot.querySelector('section');
-    const btn = this.shadowRoot.querySelector('button');
-    const p = this.shadowRoot.querySelector('p');
     const imgHref = this.getAttribute('imgHref');
+    const onclickLink = this.getAttribute('clickHref');
     const img = this.shadowRoot.querySelector('img');
     const imgBlur = this.shadowRoot.getElementById('imgBlur');
 
+    section.setAttribute('onclick', `window.location.href='${onclickLink}';`);
     img.setAttribute('src', `${imgHref}`);
     imgBlur.setAttribute('style', `background-image: url("${imgHref}");`);
-      
-    img.addEventListener('click', function() {
-        section.setAttribute('class', 'popUp');
-        btn.setAttribute('style', 'display:block;');
-        p.setAttribute('style', 'display:block;');
-    })
-    btn.addEventListener('click', function() {
-        section.setAttribute('class', 'imgCardClass');
-        btn.setAttribute('style', 'display:none;');
-        p.setAttribute('style', 'display:none;');
-    })
   }
 }
 window.customElements.define('img-card', imgCard);
